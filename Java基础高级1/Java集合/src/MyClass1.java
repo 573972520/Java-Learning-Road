2dd76f4f90bcb52cf6a93a400f7de8b56c3af61b
Carl <18767920515@163.com>
2017-05-04 21:38:58 +0800

    20170504first

---
+++ Java基础高级1/Java集合/src/MyClass1.java
@@ -0,0 +1,25 @@
+
+public class MyClass1<K, V>
+{
+	public void add(K obj)
+	{
+
+	}
+
+	//	public void hello(Object obj1, Object obj2)
+	public void hello(K obj1, V obj2)
+	{
+
+	}
+
+	//public void m2(String a)
+	public V m2(V a)
+	{
+		return a;
+	}
+
+	public void hi(V o1, K o2, K o3)
+	{
+
+	}
+}
